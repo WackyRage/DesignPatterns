cea3e2f0412296c9cafe60d06866d9d08a30c8b7
F. Tieck <franktieck-1996@live.nl>
2024-02-28 15:05:48 +0100

    Change to Empty Console Application

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
