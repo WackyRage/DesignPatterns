63e3d3a2afca96d469bf92d2f63431e154b8a3ef
F. Tieck <franktieck-1996@live.nl>
2024-03-08 12:16:51 +0100

    nuclear fix part 4

---
+++ DesignPatterns/Classes/Tournament/Log.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    internal class Log
+    {
+        public string Message { get; set; }
+        public DateTime Date {  get; set; }
+        public bool Resolved {  get; set; }
+
+        public Log(string Message, DateTime Date, bool Resolved)
+        {
+            this.Message = Message;
+            this.Date = Date;
+            this.Resolved = Resolved;
+        }
+
+        public void ChangeResolved()
+        {
+            if (Resolved)
+            {
+                Resolved = false;
+            } else
+            {
+                Resolved = true;
+            }
+        }
+
+        public string ToJSON()
+        {
+            string Date = JSONObject.ObjectToJSON(this.Date);
+            string Resolved = JSONObject.ObjectToJSON(this.Resolved);
+            List<string> list = new() { Message, Date, Resolved };
+
+            string jsonString = JSONObject.ListToJSON(list);
+            return jsonString; 
+        }
+
+        public static Log FromJSON(string jsonString)
+        {
+            List<string> list = JSONObject.JSONToList<string>(jsonString);
+            string Message = list[0];
+            DateTime Date = (DateTime)JSONObject.JSONToObject(list[1]);
+            bool Resolved = (bool)JSONObject.JSONToObject(list[2]);
+            Log Log = new(Message, Date, Resolved);
+            return Log;
+        }
+    }
+}
---
+++ DesignPatterns/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace DesignPatterns
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Clicked_Tournament(object sender, EventArgs e) 
+        {
+            Navigation.PushAsync(new TournamentOverview.TournamentOverview());
+        }
+
+        public void Button_Clicked_Main(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainMissionOverview.MainMissionOverview());
+        }
+
+        public void Button_Clicked_Secundary(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SecundaryMissionOverview.SecundaryMissionOverview());
+        }
+
+        public void Button_Clicked_Faction(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FactionOverview.FactionOverview());
+        }
+
+        public void Button_Clicked_Map(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MapOverview.MapOverview());
+        }
+    }
+
+}
