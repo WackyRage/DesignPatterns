522d9d935abe42ca22bca8b48726a66e3b09796c
Martin Westerhof <a.m.westerhof@gmail.com>
2024-03-21 15:32:44 +0100

    Update Unit.cs
    added XML documentation comments for possible SandCastle or IntelliSense

--- DesignPatterns/Classes/Faction/Unit.cs
+++ DesignPatterns/Classes/Faction/Unit.cs
@@ -7,13 +7,34 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
+    /// <summary>
+    /// Respresents a Unit in the game
+    /// <summary>
     internal interface Unit
     {
+        // Properties required by all Unit subvariants
+
+        /// <summary>
+        /// Gets and sets the name of the unit.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the unit.
+        /// </summary>
         public int Value { get; set; }
 
+        /// <summary>
+        /// Converts Unit to a Json string.
+        /// </summary>
+        /// <returns> A JSON formatted representation of the Unit.</returns>
         string ToJSON();
 
+        /// <summary>
+        /// Converts JSON format into a Unit object
+        /// </summary>
+        /// <param name="jsonString">JSON string representing the Unit object</param>
+        /// <returns>A Unit object converted from the JSON string.</returns>
         public static Unit FromJSON(string jsonString)
         {
             return (Unit)JSONObject.JSONToObject(jsonString);
