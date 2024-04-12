e94158b1bdee90e85be1b9e30a444b43b3259146
Igneous <tho09@live.nl>
2024-04-12 17:10:23 +0200

    Factions linked in front end
    Factions can now be added to the Jsons though the front end

--- DesignPatterns/FactionOverview/FactionOverview.xaml.cs
+++ DesignPatterns/FactionOverview/FactionOverview.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using static Microsoft.Maui.ApplicationModel.Permissions;
 
 namespace DesignPatterns.FactionOverview
 {
@@ -16,6 +17,27 @@ namespace DesignPatterns.FactionOverview
         public void Button_Clicked_Back(object sender, EventArgs e)
         {
             Navigation.PushAsync(new MainPage());
+        }
+
+        public void Button_Clicked_Create_Faction(object sender, EventArgs e)
+        {
+            if (factionNameEntry.Text != null)
+            {
+                Faction faction = new Faction(factionNameEntry.Text);
+                Factions.Add(faction);
+            }
+            FactionSave();
+            Navigation.PushAsync(new FactionOverview(Factions));
+        }
+
+        public void FactionSave()
+        {
+            List<string> LS = new();
+            foreach (Faction T in Factions)
+            {
+                LS.Add(T.ToJSON());
+            }
+            JSONObject.WriteJSONToFile(LS, "Factions.json");
         }
     }
 }
